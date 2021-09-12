daaf587a137881c5b907b34a5f45d947159deee6
Tanbin Hasan Ador <tanvin.hasan74@gmail.com>
2021-09-12 23:40:10 +0600

    Create A2.cpp

---
+++ Facebook Hackercup/2021/Round 1/A2.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  Tanbin_Hasan
+ *    created: 11.09.2021 23:20:52
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false); cin.tie(0);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  int BrIcToM;
+  cin >> BrIcToM;
+  for (int slania = 1; slania <= BrIcToM; ++slania) {
+    
+    cout << "Case #" << slania << ": ";
+  }
+  return 0;
+}
